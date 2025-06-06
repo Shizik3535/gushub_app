58083949aec506474cd5097802f24879749fc15c
Damir <damir.aliev3535@gmail.com>
2025-06-06 03:46:21 +0300

    Add all to build the executable file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from qt_material import apply_stylesheet
+from PyQt6.QtGui import QIcon
 
 from app.ui.windows.auth_window import AuthWindow
 from app.ui.windows.main_window import MainWindow
@@ -8,6 +9,7 @@ from app.settings import AppSettings
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("app/ui/assets/icon.ico"))
     apply_stylesheet(app, theme="dark_red.xml")
     
     # Проверяем, авторизован ли пользователь
